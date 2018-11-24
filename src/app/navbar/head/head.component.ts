d8d6fe20cbb76e1e46d58c74f7b1835c58915c89
tnpexp <32901178+tnpexp@users.noreply.github.com>
2018-11-25 05:42:02 +0700

    login

--- src/app/navbar/head/head.component.ts
+++ src/app/navbar/head/head.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { AuthService } from 'src/app/service/auth.service';
+import { GlobalService } from 'src/app/service/global.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-head',
@@ -7,12 +12,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeadComponent implements OnInit {
   public isLogin;
-  user;
-  constructor() { }
-
-  ngOnInit() {
+  public user;
+  datalist: AngularFireList<any>;
+  datauser: any[];
+  detail;
+  constructor(
+    private auth: AuthService,
+    private afAuth: AngularFireAuth,
+    private db: AngularFireDatabase,
+    private global: GlobalService,
+    private router: Router
+  ) {
+    this.afAuth.authState.subscribe(data => {
+      this.detail = this.db
+        .list('/user', ref => ref.orderByChild('email').equalTo(data.email))
+        .valueChanges();
+      console.log(data.email);
+      this.detail.subscribe(snap => {
+        snap.forEach(element => {
+          this.user = element.fname + ' ' + element.lname;
+          this.global.setUser(element.users);
+        });
+      });
+    });
+    this.auth.cookieAuth().subscribe(data => {
+      this.isLogin = data;
+    });
   }
 
   logout() {
+    this.auth.logout();
+    location.reload();
   }
+
+  editprofile() {
+    this.router.navigate(['/editprofile']);
+  }
+  ngOnInit() {}
 }
