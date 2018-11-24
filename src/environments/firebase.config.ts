a476992d0eb96c37309e8cae16818891654985cf
tnpexp <32901178+tnpexp@users.noreply.github.com>
2018-11-25 05:32:15 +0700

    firebase

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: 'AIzaSyAZC4OS3ooI26df9XpOJDj8zSwVxj8mUis',
+    authDomain: 'farmproject-831d7.firebaseapp.com',
+    databaseURL: 'https://farmproject-831d7.firebaseio.com',
+    projectId: 'farmproject-831d7',
+    storageBucket: 'farmproject-831d7.appspot.com',
+    messagingSenderId: '40438337476'
+  };
