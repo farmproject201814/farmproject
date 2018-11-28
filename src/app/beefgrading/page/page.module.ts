e77c03613665959938c73c5bb2c0232dfcec0788
tnpexp <32901178+tnpexp@users.noreply.github.com>
2018-11-28 15:33:58 +0700

    add component

---
+++ src/app/beefgrading/page/page.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PageRoutingModule } from './page-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    PageRoutingModule
+  ],
+  declarations: []
+})
+export class PageModule { }
